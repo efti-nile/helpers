3bad8d1374ef03099c5fb0911c50903dfdbb6b81
Efti Nile <efti.nile@gmail.com>
2021-01-17 11:04:47 +0800

    Dummy Message

--- git_aliases.sh
+++ git_aliases.sh
@@ -3,6 +3,7 @@ alias ga='git add'
 alias gc='git commit'
 alias gcm='git commit -m'
 alias gca='git commit --amend'
+alias gdnc='git add *; git commit -m "Dummy Message"'  # git do not care
 alias gph='git push'
 alias gpl='git pull'
 alias gd='git diff'
