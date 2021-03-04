b49a974fda7b228e92460f7c1a421a3ce568493f
Efti Nile <efti.nile@gmail.com>
2021-03-05 05:20:19 +0800

    Update ipython_config.py

--- ipython_config.py
+++ ipython_config.py
@@ -2,5 +2,12 @@ c = get_config()
 
 # Run all nodes interactively
 # In Jupyter it allows to print several out variable
-# from single cell
+# from single cell.
+# x, y = 0, 1
+# x;
+# y;<Enter>
+# prints
+# 1
+# 0
+# 2 vars at a time! woh!
 c.InteractiveShell.ast_node_interactivity = "all"
