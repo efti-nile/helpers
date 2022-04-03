d5941b48cdd2e9b46e0e3bc3ec2cb20a63218fd2
Efti Nile <efti.nile@gmail.com>
2022-04-03 18:09:33 +0300

    Update infection.sh

--- infection.sh
+++ infection.sh
@@ -9,12 +9,12 @@ usermod -aG sudo ft
 
 apt -y install \
     zsh        \
-	tree       \
-	tmux       \
-	vim        \
-	git        \
-	snapd
-	
+    tree       \
+    tmux       \
+    vim        \
+    git        \
+    snapd
+    
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -30,15 +30,15 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt -y install    \
-	docker-ce     \
-	docker-ce-cli \
-	containerd.io 
+    docker-ce     \
+    docker-ce-cli \
+    containerd.io 
 
 # Install Desktop Environment
 apt -y install
-	xfce4
-	xfce4-goodies
-	firefox
+    xfce4
+    xfce4-goodies
+    firefox
 
 su ft
 
